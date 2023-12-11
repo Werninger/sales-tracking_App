1342e28f7d0f0dc30593491d11694c4656b7fbf5
Werninger <evawerninger@gmail.com>
2023-12-11 20:28:12 +0100

    add possibility to name products per input

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'dart:async';
 
 void main() {
   runApp(MyApp());
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         title: 'sales-tracking_App',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
         ),
         home: MyHomePage(),
       ),
@@ -26,9 +26,26 @@ class MyApp extends StatelessWidget {
 }
 
 class MyAppState extends ChangeNotifier {
-  String product1_Name = "Product 1";
-  String product2_Name = "Product 2";
-  String product3_Name = "Product 3";
+  String product1_name = "Product 1";
+  String product2_name = "Product 2";
+  String product3_name = "Product 3";
+
+  void setName(String s, String name){
+    switch (name){
+      case 'p1':
+        product1_name = s;
+        break;
+      case 'p2':
+        product2_name = s;
+        break;
+      case 'p3':
+        product3_name = s;
+        break;
+      default:
+        break;
+    }
+    notifyListeners();
+  }
 
   var product1_curr = 0;
   var product2_curr = 0;
@@ -38,8 +55,8 @@ class MyAppState extends ChangeNotifier {
   var product2 = 0;
   var product3 = 0;
 
-  var babPrice = 16;
-  var toPay = 0;
+  double price_bab = 16.00;
+  double toPay = 0.0;
 
   bool showPayButton = false;
 
@@ -62,7 +79,7 @@ class MyAppState extends ChangeNotifier {
       default:
         break;
     }
-    toPay += babPrice;
+    toPay += price_bab;
     notifyListeners();
   }
   void updateVariables(){
@@ -74,17 +91,22 @@ class MyAppState extends ChangeNotifier {
     product1_curr = 0;
     product2_curr = 0;
     product3_curr = 0;
-    toPay = 0;
+    toPay = 0.0;
     notifyListeners();
   }
 
   var cash = 0;
   var twint = 0;
   
-  var totalIncome = 0;
+  double totalIncome = 0.0;
   void addToIncome(){
     totalIncome += toPay;
   }  
+
+  double totalOTH = 0.0; // On The House
+  void addToOTH(){
+    totalOTH += toPay;
+  }
 }
 
 class MyHomePage extends StatefulWidget {
@@ -106,6 +128,9 @@ class _MyHomePageState extends State<MyHomePage> {
       case 1 :
         page = OverviewPage();
         break;
+      case 2 :
+        page = SettingPage();
+        break;
       default:
         throw UnimplementedError('no widget for $selectedIndex');
     }
@@ -117,7 +142,7 @@ class _MyHomePageState extends State<MyHomePage> {
               SafeArea(
                 child: NavigationRail(
                   extended: constraints.maxWidth >= 600,
-                  destinations: [
+                  destinations: const [
                     NavigationRailDestination(
                       icon: Icon(Icons.shopping_bag_outlined),
                       label: Text('Verkauf'),
@@ -126,6 +151,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       icon: Icon(Icons.home),
                       label: Text('Overview'),
                     ),
+                    NavigationRailDestination(icon: Icon(Icons.settings), label: Text('Settings'))
                   ],
                   selectedIndex: selectedIndex,
                   onDestinationSelected: (value) {
@@ -167,17 +193,17 @@ class GeneratorPage extends StatelessWidget {
             children: [
               ElevatedButton(
                 onPressed: () {appState.add1('product1');},
-                child: Text('${appState.product1_Name}: ${appState.product1_curr}'),
+                child: Text('${appState.product1_name}: ${appState.product1_curr}'),
               ),
               SizedBox(width: 10),
               ElevatedButton(
                     onPressed: () {appState.add1('product2');},
-                    child: Text('${appState.product2_Name}: ${appState.product2_curr}'),
+                    child: Text('${appState.product2_name}: ${appState.product2_curr}'),
               ),
               SizedBox(width: 10),
               ElevatedButton(
                 onPressed: () {appState.add1('product3');},
-                child: Text('${appState.product3_Name}: ${appState.product3_curr}'),
+                child: Text('${appState.product3_name}: ${appState.product3_curr}'),
               ),
             ],
           ), 
@@ -187,12 +213,18 @@ class GeneratorPage extends StatelessWidget {
             children: [
               ElevatedButton(
                 onPressed: (){appState.resetVariables();},
-                child: Text('Reset'),
+                child: const Row(children: [Icon(Icons.restart_alt), Text('Reset'),],),
               ),
               const SizedBox(width: 10),
               ElevatedButton(
                 onPressed: () {appState.showPayButtonF();},
-                child: Text('Pay: ${appState.toPay}'),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.shopping_cart_checkout), // Add the payment icon
+                    Text('Pay: ${appState.toPay}'),
+                  ],
+                ),
               ),
             ],
           ),
@@ -203,17 +235,24 @@ class GeneratorPage extends StatelessWidget {
               children: [
                 ElevatedButton(
                   onPressed: (){appState.updateVariables();appState.addToIncome(); appState.resetVariables();appState.showPayButtonF();},
-                  child: const Text('Cash'),
+                  child: const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.money), // Add the payment icon
+                      SizedBox(width: 8), // Add some spacing between the icon and text
+                      Text('Cash'),
+                    ],
+                  ),
                 ),
                 const SizedBox(width: 10),
                 ElevatedButton(
                   onPressed: () {appState.updateVariables();appState.addToIncome(); appState.resetVariables();appState.showPayButtonF();},
-                  child: const Text('Twint'),
+                  child: const Row(children: [Icon(Icons.hexagon_outlined),Text('Twint'),],)
                 ),
                 const SizedBox(width: 10),
                 ElevatedButton(
-                  onPressed: () {appState.updateVariables(); appState.resetVariables();appState.showPayButtonF();},
-                  child: const Text('On the House'),
+                  onPressed: () {appState.updateVariables(); appState.addToOTH(); appState.resetVariables();appState.showPayButtonF();},
+                  child: const Row(children: [Icon(Icons.home),Text('On the house')],)
                 ),
               ],
             ),
@@ -234,17 +273,76 @@ class OverviewPage extends StatelessWidget {
           padding: const EdgeInsets.all(20),
           child: Column(
             children: [
-              Text('Total ${appState.product1_Name}: ${appState.product1}'),
-              Text('Total ${appState.product2_Name}: ${appState.product2}'),
-              Text('Total ${appState.product3_Name}: ${appState.product3}')
+              Text('Total ${appState.product1_name}: ${appState.product1}'),
+              Text('Total ${appState.product2_name}: ${appState.product2}'),
+              Text('Total ${appState.product3_name}: ${appState.product3}'),
+              const SizedBox(height: 50),
+              Text('Total income: ${appState.totalIncome}'),
+              Text('Total on the house: ${appState.totalOTH}'),
+
             ],
           ),
         ),
-        Center(
-          child: Text('Total income: ${appState.totalIncome}'),
-        )
       ],
     );
   }
 }
 
+class SettingPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ElevatedButton(
+          onPressed: () async {String input = await _showInputDialog(context, 'Name of Product 1'); appState.setName(input, 'p1');}, 
+          child: Text('Name of ${appState.product1_name}'),),
+        const SizedBox(height: 10),
+        ElevatedButton(
+          onPressed: () async {String input = await _showInputDialog(context, 'Name of Product 1'); appState.setName(input, 'p2');}, 
+          child: Text('Name of ${appState.product2_name}'),),
+        const SizedBox(height: 10),
+        ElevatedButton(
+          onPressed: () async {String input = await _showInputDialog(context, 'Name of Product 1'); appState.setName(input, 'p3');}, 
+          child: Text('Name of ${appState.product3_name}'),),
+      ],
+    );
+  }
+}
+
+Future<dynamic> _showInputDialog(BuildContext context, String s) async {
+    TextEditingController controller = TextEditingController();
+
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(s),
+          content: TextField(
+            controller: controller,
+            decoration: InputDecoration(
+              hintText: 'Type...',
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                // Get the user input as a string
+                String userInput = controller.text;
+                Navigator.of(context).pop(userInput);
+              },
+              child: Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+  }
